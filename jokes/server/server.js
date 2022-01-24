03cd4220169c4ff11aad403d706d725efa2e423a
willd <willdwelle@frontier.com>
2022-01-24 14:04:09 -0800

    added routes

--- jokes/server/server.js
+++ jokes/server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json(), express.urlencoded({ extended: true }));
 // This will fire our mongoose.connect statement to initialize our database connection
 require("./server/config/mongoose.config");
+
+const AllMyJokeRoutes = require("./server/routes/joke.routes");
+AllMyJokeRoutes(app);
 
 app.listen(8000, () => console.log("The server is all fired up on port 8000"));
